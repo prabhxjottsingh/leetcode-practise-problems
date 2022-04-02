2e826851408d9787dff60ad35700cea845e2a2f1
Prabhxjott <prabhx.jott@gmail.com>
2022-04-02 15:54:06 +0530

    Valid Plaindrome - II

--- String/0680 - Valid Palindrome II.cpp
+++ String/0680 - Valid Palindrome II.cpp
@@ -1,26 +1,32 @@
-//Problem Link: https://leetcode.com/problems/valid-palindrome-ii/
+// Problem Link: https://leetcode.com/problems/valid-palindrome-ii/
 
-//Time Complexity: O(N)
-//Space Complexity: O(1)
+// Time Complexity: O(N)
+// Space Complexity: O(1)
 
-class Solution {
+class Solution
+{
 private:
-    bool check(int left, int right, string s){
-        while(left < right){
-            if(s[left] != s[right])
+    bool check(int left, int right, string s)
+    {
+        while (left < right)
+        {
+            if (s[left] != s[right])
                 return false;
             left++;
             right--;
         }
         return true;
     }
-    
+
 public:
-    bool validPalindrome(string s) {
+    bool validPalindrome(string s)
+    {
         int left = 0, right = s.size() - 1;
-        while(left < right){
-            if(s[left] != s[right]){
-                if( (!check(left + 1, right, s)) && (!check(left, right - 1, s)) )
+        while (left < right)
+        {
+            if (s[left] != s[right])
+            {
+                if ((!check(left + 1, right, s)) && (!check(left, right - 1, s)))
                     return false;
                 else
                     return true;
