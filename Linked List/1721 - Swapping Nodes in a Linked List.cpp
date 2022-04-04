286df32e073084c9aaac1198fcf8586d45aae91f
Prabhxjott <prabhx.jott@gmail.com>
2022-04-04 17:39:31 +0530

    Swap Nodes and K pos from end and begin

--- Linked List/1721 - Swapping Nodes in a Linked List.cpp
+++ Linked List/1721 - Swapping Nodes in a Linked List.cpp
@@ -1,57 +1,63 @@
-//Tags: Medium, Linked List
-//Problem Link: https://leetcode.com/problems/swapping-nodes-in-a-linked-list/
-//Time Complexity: O(N)
-//Space Complexity: Constant
+// Tags: Medium, Linked List
+// Problem Link: https://leetcode.com/problems/swapping-nodes-in-a-linked-list/
+// Time Complexity: O(N)
+// Space Complexity: Constant
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution
 {
+private:
+    int countNodes(ListNode *head)
+    {
+        int count = 0;
+        ListNode *temp = head;
+        while (temp)
+        {
+            temp = temp->next;
+            count++;
+        }
+        return count;
+    }
+
+    int findNodeK(ListNode *head, int k)
+    {
+        ListNode *temp = head;
+        int res = 0, x = 0;
+        while (temp)
+        {
+            x++;
+            if (x == k)
+            {
+                res = temp->val;
+                break;
+            }
+            temp = temp->next;
+        }
+        return res;
+    }
+
+    void changeVal(ListNode *&head, int pos, int newVal)
+    {
+        ListNode *temp = head;
+        int currPos = 0;
+        while (temp)
+        {
+            currPos++;
+            if (currPos == pos)
+                temp->val = newVal;
+            temp = temp->next;
+        }
+    }
+
 public:
     ListNode *swapNodes(ListNode *head, int k)
     {
-        int kFront = 0, KRear = k, kFront2, kRear2;
-        int kFrontInteger = 0, kRearInteger = 0, count = 0;
-        ListNode *temp1 = head, *temp2 = head, *temp3 = head;
-        while (temp1)
-        {
-            kFront++;
-            count++;
-            if (kFront == k)
-            {
-                kFrontInteger = temp1->val;
-            }
-            temp1 = temp1->next;
-        }
-        KRear = kRear2 = count - k;
-        while (temp2)
-        {
-            KRear--;
-            if (KRear == -1)
-            {
-                kRearInteger = temp2->val;
-            }
-            temp2 = temp2->next;
-        }
-        kRear2++;
-        while (temp3)
-        {
-            k--;
-            kRear2--;
-            if (k == 0)
-                temp3->val = kRearInteger;
-            if (kRear2 == 0)
-                temp3->val = kFrontInteger;
-            temp3 = temp3->next;
-        }
+        int count = countNodes(head);
+        int kFromEnd = count - k + 1;
+        int kFromBegin = k;
+        int kNodeValFromBegin = findNodeK(head, kFromBegin);
+        int KNodeValFromEnd = findNodeK(head, kFromEnd);
+        changeVal(head, kFromBegin, KNodeValFromEnd);
+        changeVal(head, kFromEnd, kNodeValFromBegin);
         return head;
     }
 };
