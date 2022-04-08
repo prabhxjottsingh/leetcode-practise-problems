d2c7cfab4df0d0705afc04f736407d29dfe660f1
Prabhxjott <prabhx.jott@gmail.com>
2022-04-08 10:10:57 +0530

    Kth Largest Element in a stream

--- Heap/0703 - Kth Largest Element in a Stream.cpp
+++ Heap/0703 - Kth Largest Element in a Stream.cpp
@@ -1,1 +1,37 @@
-//Problem Link: https://leetcode.com/problems/kth-largest-element-in-a-stream/
+// Problem Link: https://leetcode.com/problems/kth-largest-element-in-a-stream/
+
+// Time Complexity: O(N * log N)
+// Space Complexity: O(N)
+
+class KthLargest
+{
+private:
+    priortiy_queue < int, vector<int>, greater<int> minHeap;
+    int K;
+
+public:
+    KthLargest(int k, vector<int> &nums)
+    {
+        K = k;
+        for (auto &a : nums)
+        {
+            minHeap.push(a);
+            if (minHeap.size() > k)
+                minHeap.pop();
+        }
+    }
+
+    int add(int val)
+    {
+        minHeap.push(val);
+        if (minHeap.size() > K)
+            minHeap.pop();
+        return minHeap.top() :
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
