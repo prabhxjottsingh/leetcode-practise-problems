12776e96d8706f2476d75921640c753183d25565
Prabhjot Singh <Prabhx.jott@gmail.com>
2022-07-25 12:06:56 +0530

    Find first and last Pos - BS

--- Binary Search/0034 - Find First and Last Position of Element in Sorted Array.cpp
+++ Binary Search/0034 - Find First and Last Position of Element in Sorted Array.cpp
@@ -1,3 +1,7 @@
+// Problem Link: https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+// Time Complexity: O(log N)
+// Space Complexit: O(1)
+
 class Solution
 {
 private:
@@ -32,5 +36,42 @@ public:
         resVec.push_back(findPos(nums, target, true));
         resVec.push_back(findPos(nums, target, false));
         return resVec;
+    }
+};
+
+
+
+class Solution
+{
+private:
+    int findPos(int low, int high, vector<int> &nums, int target, bool firstPos)
+    {
+        int res = -1;
+        while (low < high)
+        {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target)
+            {
+                res = mid;
+                if (firstPos)
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            }
+            else if (nums[mid] < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return res;
+    }
+
+public:
+    vector<int> searchRange(vector<int> &nums, int target)
+    {
+        int n = nums.size();
+        int firstPos = findPos(0, n - 1, nums, target, 1);
+        int lastPos = findPos(0, n - 1, nums, target, 0);
+        return {firstPos, lastPos};
     }
 };
