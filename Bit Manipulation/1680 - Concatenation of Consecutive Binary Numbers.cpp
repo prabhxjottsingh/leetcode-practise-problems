ed079b1feb79bece35bdd582d8c221d8022bd77c
Prabhjot Singh <Prabhx.jott@gmail.com>
2022-09-23 14:03:20 +0530

    Need to Work on Bit Manipulation

---
+++ Bit Manipulation/1680 - Concatenation of Consecutive Binary Numbers.cpp
@@ -0,0 +1,20 @@
+// Problem Link: https://leetcode.com/problems/concatenation-of-consecutive-binary-numbers/
+
+// Time Complexity: O(N * K)
+// Space Complexity: O(`)
+
+class Solution
+{
+public:
+    int concatenatedBinary(int n)
+    {
+        long long res = 0;
+        const int mod = 1e9 + 7;
+        for (int i = 1; i <= n; i++)
+        {
+            int sz = (int)(log2(i) + 1);
+            res = (((res << sz) % mod) + (i % mod)) % mod;
+        }
+        return res;
+    }
+};
