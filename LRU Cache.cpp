45b5788342bd5996ecc9815fdf9a84aa494e9c5d
Prabhjot Singh <Prabhx.jott@gmail.com>
2023-01-30 20:58:30 +0530

    used array instead of maps

--- LRU Cache.cpp
+++ LRU Cache.cpp
@@ -1,61 +1,56 @@
 // Tc: O(N) //Linked List deletion and insertion is O(1), and it is done N times
 // sc: O(n)
 
-class LRUCache
-{
+class LRUCache {
 private:
-    class Node
-    {
+    class Node{
     public:
         int key;
         int value;
-        Node *prev = NULL;
-        Node *next = NULL;
+        Node* prev = NULL;
+        Node* next = NULL;
 
-        Node(int key, int value)
-        {
+        Node(int key, int value){
             this->key = key;
             this->value = value;
         }
     };
 
-    Node *head = new Node(-1, -1);
-    Node *tail = new Node(-1, -1);
+    Node* head = new Node(-1, -1);
+    Node* tail = new Node(-1, -1);
     int cap;
-    unordered_map<int, Node *> hash;
+    static const int N = 1e4 + 7;
+    Node* hash[N];
+    int countSz = 0;
 
-    void addNode(Node *newNode)
-    {
-        Node *nextNode = head->next;
+    void addNode(Node* newNode){
+        Node* nextNode = head->next;
         nextNode->prev = newNode;
         head->next = newNode;
         newNode->next = nextNode;
         newNode->prev = head;
     }
 
-    void deleteNode(Node *nodeToBeDeleted)
-    {
-        Node *prevSaveNode = nodeToBeDeleted->prev;
-        Node *nextSavedNode = nodeToBeDeleted->next;
+    void deleteNode(Node* nodeToBeDeleted){
+        Node* prevSaveNode = nodeToBeDeleted->prev;
+        Node* nextSavedNode = nodeToBeDeleted->next;
         nextSavedNode->prev = prevSaveNode;
         prevSaveNode->next = nextSavedNode;
         // delete(nodeToBeDeleted);
     }
 
 public:
-    LRUCache(int capacity)
-    {
+    LRUCache(int capacity) {
         cap = capacity;
         head->next = tail;
         tail->prev = head;
+        for(int i = 0; i < N; i++) hash[i] = NULL;
     }
-
-    int get(int key)
-    {
-        if (hash.count(key))
-        {
+    
+    int get(int key) {
+        if( hash[key] != NULL ){
             // cout << hash[key]->value;
-            Node *tarNode = hash[key];
+            Node* tarNode = hash[key];
             int resVal = tarNode->value;
             deleteNode(tarNode);
             addNode(tarNode);
@@ -64,27 +59,23 @@ public:
         }
         return -1;
     }
-
-    void put(int key, int value)
-    {
-        if (hash.count(key))
-        {
-            Node *tarNode = hash[key];
+    
+    void put(int key, int value) {
+        if( hash[key] != NULL ){
+            Node* tarNode = hash[key];
             deleteNode(tarNode);
             addNode(new Node(key, value));
             hash[key] = head->next;
         }
-        else
-        {
-            if (cap == hash.size())
-            {
-                hash.erase(tail->prev->key);
+        else{
+            if( cap == countSz ){
+                hash[tail->prev->key] = NULL;
                 deleteNode(tail->prev);
                 addNode(new Node(key, value));
                 hash[key] = head->next;
             }
-            else
-            {
+            else{
+                countSz++;
                 addNode(new Node(key, value));
                 hash[key] = head->next;
             }
